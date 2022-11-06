787649fccbe223baec16bd27768ae69ed366a37c
Luanasousa1505 <106691446+Luanasousa1505@users.noreply.github.com>
2022-11-06 14:47:47 -0300

    Add files via upload

---
+++ init.php
@@ -0,0 +1,13 @@
+<?php
+
+//timezone
+
+date_default_timezone_set('America/sao_paulo');
+
+// conexão com o banco de dados
+
+define('BD_SERVIDOR','localhost');
+define('BD_USUARIO','root');
+define('BD_SENHA','akame');
+define('BD_BANCO','livraria');
+
